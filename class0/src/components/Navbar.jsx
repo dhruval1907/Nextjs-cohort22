7c21e9eb68495d2cb03194fefd51d2a86716fb87
dhruval1907 <dhruvaldalwadi1907@gmail.com>
2026-01-17 20:20:53 +0530

    navbarexport

--- class0/src/components/Navbar.jsx
+++ class0/src/components/Navbar.jsx
@@ -1,11 +1,23 @@
-import React from 'react'
+import Link from "next/link";
+import React from "react";
 
 const Navbar = () => {
   return (
-    <div className='flex justify-between h-20 w-full bg-blue-300'>
-                
+    <div className="text-black flex justify-between items-center h-20 w-full bg-blue-300">
+      <h1 className="font-semibold text-2xl">Sheriynas</h1>
+      <div className="flex items-center gap-5">
+        <Link className="font-semibold text-2xl" href="/about">
+          About
+        </Link>
+        <Link className="font-semibold text-2xl" href="/contact">
+          Contact
+        </Link>
+        <Link className="font-semibold text-2xl" href="/courses">
+          courses
+        </Link>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
