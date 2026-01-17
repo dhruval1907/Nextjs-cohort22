50088fa58f16af793f332998b0b9f89171049f63
dhruval1907 <dhruvaldalwadi1907@gmail.com>
2026-01-17 20:34:14 +0530

    routing completed

--- class0/src/components/Navbar.jsx
+++ class0/src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import React from "react";
 const Navbar = () => {
   return (
     <div className="text-black flex justify-between items-center h-20 w-full bg-blue-300">
-      <h1 className="font-semibold text-2xl">Sheriynas</h1>
+      <h2 className="font-semibold text-2xl">Sheriynas</h2>
       <div className="flex items-center gap-5">
         <Link className="font-semibold text-2xl" href="/about">
           About
