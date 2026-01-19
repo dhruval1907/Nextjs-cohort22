22cae8b61cabdc9a4e49474b46ffeffe122440ff
dhruval1907 <dhruvaldalwadi1907@gmail.com>
2026-01-19 18:17:57 +0530

    grouprouteq

--- class1/app/components/Navbar.jsx
+++ class1/app/components/Navbar.jsx
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import React from 'react'
 
 const Navbar = () => {
@@ -5,10 +6,10 @@ const Navbar = () => {
     <div className='flex items-center justify-between p-10 h-20 w-full bg-blue-800'>
         <h2 className='text-2xl font-semibold'>Sheriyans</h2>
         <div className='flex items-center gap-10 font-semibold  text-xl'>
-            <a href="">Home</a>
-            <a href="">About</a>
-            <a href="">Contact</a>
-            <a href="">Courses</a>
+            <Link href="/">Home</Link>
+            <Link href="/about">About</Link>
+            <Link href="/contact">Contact</Link>
+            <Link href="/course">Courses</Link>
         </div>
     </div>
   )
